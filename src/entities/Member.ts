cbe250716849ab67941107a2186dbe5cfdb5624b
araaki12345 <74499464+araaki12345@users.noreply.github.com>
2024-05-01 10:55:21 +0900

    feat:メンバの状態を管理する機能を実装

---
+++ src/entities/Member.ts
@@ -0,0 +1,17 @@
+class Member{
+    id:string;
+    name:string;
+    email:string;
+    isAuthorised:boolean;
+
+    constructor(id: string, name: string) {
+        this.id = id;
+        this.name = name;
+        this.email = "";
+        this.isAuthorised = false;
+    }
+
+    authorise(){
+        this.isAuthorised = true;
+    }
+}
