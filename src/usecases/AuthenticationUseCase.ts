10e0ece2ef5494dcf91ba79b6bb15d8ef93590c8
araaki12345 <74499464+araaki12345@users.noreply.github.com>
2024-05-02 11:19:39 +0900

    feat:メール送信に関するプログラムを実装

---
+++ src/usecases/AuthenticationUseCase.ts
@@ -0,0 +1,26 @@
+import { Member } from "../entities/Member";
+import { MemberRepository } from "../interfaces/MemberRepository";
+
+class AuthenticationUseCase {
+  private memberRepository: MemberRepository;
+
+  constructor(memberRepository: MemberRepository) {
+    this.memberRepository = memberRepository;
+  }
+
+  async handleNewMember(id: string): Promise<void> {
+    const member = new Member(id, "");
+    await this.memberRepository.save(member);
+  }
+  async authenticateMember(memberId: string, email: string): Promise<boolean> {
+    const member = await this.memberRepository.findById(memberId);
+    if (!member) return false;
+    if (email.endsWith("@shizuoka.ac.jp")) {
+      member.setEmail(email);
+      member.authorise();
+      await this.memberRepository.update(member);
+      return true;
+    }
+    return false;
+  }
+}
