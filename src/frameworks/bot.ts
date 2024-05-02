db55850cc0c89355d63b29b99b43e165bd411584
araaki12345 <74499464+araaki12345@users.noreply.github.com>
2024-05-02 16:00:29 +0900

    feat: フレームワーク部分を実装

---
+++ src/frameworks/bot.ts
@@ -0,0 +1,25 @@
+import { Client, GatewayIntentBits } from "discord.js";
+import { AuthenticationUseCase } from "../usecases/AuthenticationUseCase";
+import { InMemoryMemberRepository } from "../interfaces/InMemoryMemberRepository";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent, // メッセージ内容を受信するためにはこのインテントが必要
+    GatewayIntentBits.DirectMessages,
+  ],
+});
+
+client.on("ready", () => {
+  if (client.user) {
+    console.log(`Logged in as ${client.user.tag}!`);
+  } else {
+    console.log("Client is ready, but the user detail is not available.");
+  }
+});
+client.on("guildMemberAdd", async (member) => {
+  console.log(`New member added:" ${member.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
