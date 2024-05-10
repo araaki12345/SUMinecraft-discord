aa17dbbb4f318399a1a3a61e17e7914f631e28dd
araaki12345 <74499464+araaki12345@users.noreply.github.com>
2024-05-10 09:20:51 +0900

    feat: 新規加入者に対し「未認証」ロールを付与する機能を実装

--- src/frameworks/bot.ts
+++ src/frameworks/bot.ts
@@ -36,6 +36,25 @@ client.on("ready", () => {
 
 client.on("guildMemberAdd", async (member) => {
   console.log(`New member added:" ${member.user.tag}`);
+  let unaurhorizedRole = member.guild.roles.cache.find(
+    (role) => role.name === "Unauthorized"
+  );
+  if (!unaurhorizedRole) {
+    try {
+      unaurhorizedRole = await member.guild.roles.create({
+        name: "未認証",
+        color: "Grey",
+        reason: "未認証ユーザー用のロール",
+      });
+      console.log("未認証ロールの作成に成功");
+    } catch (error) {
+      console.error("未認証ロールの作成に失敗", error);
+    }
+  }
+  if (unaurhorizedRole) {
+    member.roles.add(unaurhorizedRole);
+    console.log(`未認証ロールが ${member.user.tag}に付与されました。`);
+  }
   await authUseCase.handleNewMember(member.id);
   console.log(`Sent a welcome message to ${member.user.tag}.`);
 });
