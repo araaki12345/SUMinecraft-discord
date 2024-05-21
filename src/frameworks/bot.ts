946a164c2b6258bfe2c89c4c29ebc801f36d1824
araaki12345 <74499464+araaki12345@users.noreply.github.com>
2024-05-21 19:10:03 +0900

    feat: トークンの確認を行い、エラーを防止する機能を実装

--- src/frameworks/bot.ts
+++ src/frameworks/bot.ts
@@ -12,6 +12,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+if (!DISCORD_TOKEN) {
+  throw new Error("Discordトークンが設定されていません");
+}
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
